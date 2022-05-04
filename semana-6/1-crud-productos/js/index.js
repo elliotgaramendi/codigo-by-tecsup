2842285d852ce443739ca9398827c92cdcb669ed
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-03 22:56:25 -0500

    s6.1: Update js index createProduct

--- semana-6/1-crud-productos/js/index.js
+++ semana-6/1-crud-productos/js/index.js
@@ -9,6 +9,8 @@ const documentReady = () => {
   ];
 
   const tBodyProducto = document.querySelector('#tBodyProducto');
+  const formProducto = document.querySelector('#formProducto');
+  const contenedorError = document.querySelector('#contenedorError');
 
   productos.forEach((element) => {
     const { id, nombre, precio, marca, categoria, stock } = element;
@@ -16,13 +18,33 @@ const documentReady = () => {
       <tr>
         <th>${id}</th>
         <td>${nombre}</td>
-        <td>${precio}</td>
+        <td>${precio.toLocaleString('es-PE', { style: 'currency', currency: 'PEN', minimumFractionDigits: 2 })}</td>
         <td>${marca}</td>
         <td>${categoria}</td>
         <td>${stock}</td>
       </tr>
     `
   });
+
+  // Create
+  const createProduct = (e) => {
+    e.preventDefault();
+    const documentFormProducto = document.forms['formProducto'];
+    const nombre = documentFormProducto['nombre'].value;
+    const precio = documentFormProducto['precio'].value;
+    const marca = documentFormProducto['marca'].value;
+    const categoria = documentFormProducto['categoria'].value;
+    const stock = documentFormProducto['stock'].value;
+
+    if ([nombre.trim(), precio.trim(), marca.trim(), categoria.trim(), stock.trim()].includes('')) {
+      contenedorError.innerHTML = 'Completar todos los campos';
+    } else {
+      contenedorError.innerHTML = '';
+      alert('Producto creado');
+    }
+  };
+
+  formProducto.addEventListener('submit', createProduct);
 }
 
 document.addEventListener('DOMContentLoaded', documentReady);
