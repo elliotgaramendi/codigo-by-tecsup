747e86861e263967c446724b9bcb3d37fff89ba1
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-07 11:03:45 -0500

    s6.3: Update js models

--- semana-6/3-dashboard-feliz/js/models.js
+++ semana-6/3-dashboard-feliz/js/models.js
@@ -1,13 +1,28 @@
 export const models = () => {
+  // Chart.defaults.color = '#fff';
   const dashboardModelsChart = document.getElementById('dashboardModelsChart');
   const data = {
     labels: ['JavaScript', 'HTML', 'CSS'],
     datasets: [{
-      data: [60, 50, 50]
+      data: [60, 50, 50],
+      borderColor: ['red', 'green', 'blue'],
+      backgroundColor: ['#0008', 'brown', 'skyblue']
     }]
   };
 
-  new Chart(dashboardModelsChart, { type: 'doughnut', data });
+  const options = {
+    plugins: {
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }
+
+  new Chart(dashboardModelsChart, {
+    type: 'doughnut',
+    data,
+    options
+  });
 }
 
 export default models;
