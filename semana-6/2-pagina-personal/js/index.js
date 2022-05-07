0a8e94f9489c13ed104ebb4a50690c42cde45681
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-06 22:49:15 -0500

    s6.2: Update html js profile & util portafolio

--- semana-6/2-pagina-personal/js/index.js
+++ semana-6/2-pagina-personal/js/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-import { contacts } from "../utils/contacts.js";
+import { contacts as contactsData, profile as profileData, technologies as technologiesData } from "../utils/portafolio.js";
 import footer from "./footer.js";
 import header from "./header.js";
 import profile from "./profile.js";
 
 const documentReady = () => {
   header();
-  profile();
-  footer(contacts);
+  profile(profileData, technologiesData);
+  footer(contactsData);
 }
 
 document.addEventListener('DOMContentLoaded', documentReady);
--- semana-6/2-pagina-personal/js/profile.js
+++ semana-6/2-pagina-personal/js/profile.js
@@ -1,6 +1,26 @@
-const profile = () => {
+const profile = (profileData, technologiesData) => {
+  const { name, shortName, slogan, description, urlPhoto, urlCv } = profileData;
+
   const profileInfo = document.querySelector('.profile__info');
   const profileFigure = document.querySelector('.profile__figure');
+
+  document.querySelector('.profile__info-title').innerHTML = name;
+  document.querySelector('.profile__info-subtitle').innerHTML = slogan;
+  document.querySelector('.profile__info-description').innerHTML = description;
+  document.querySelector('.profile__info-link').innerHTML = 'CV';
+  document.querySelector('.profile__info-link').href = urlCv;
+  document.querySelector('.profile__info-link[href="#projects"]').innerHTML = 'Proyectos';
+
+  document.querySelector('.profile__figure-image').src = urlPhoto;
+  document.querySelector('.profile__figure-image').alt = shortName;
+  document.querySelector('.profile__figure-tech-one').src = technologiesData[0].image;
+  document.querySelector('.profile__figure-tech-one').alt = technologiesData[0].name;
+  document.querySelector('.profile__figure-tech-two').src = technologiesData[1].image;
+  document.querySelector('.profile__figure-tech-two').alt = technologiesData[1].name;
+  document.querySelector('.profile__figure-tech-three').src = technologiesData[2].image;
+  document.querySelector('.profile__figure-tech-three').alt = technologiesData[2].name;
+  document.querySelector('.profile__figure-tech-four').src = technologiesData[3].image;
+  document.querySelector('.profile__figure-tech-four').alt = technologiesData[3].name;
 
   const documentScroll = () => {
     profileInfo.style.marginTop = `-${scrollY}px`;
