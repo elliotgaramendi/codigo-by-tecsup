983903c8fb0a5ee65eb3b846e56ae79e9869fb54
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-06 20:26:22 -0500

    6.2: Update html css js theme color

--- semana-6/2-pagina-personal/js/header.js
+++ semana-6/2-pagina-personal/js/header.js
@@ -1,15 +1,23 @@
 const header = () => {
   const header = document.querySelector('.header');
   const headerNav = document.querySelector('.header-nav');
+  
+  const headerNavThemeIconContainer = document.getElementById('headerNavThemeIconContainer');
 
   const headerNavMenuIcon = document.getElementById('headerNavMenuIcon');
   const headerNavMenuCloseIcon = document.getElementById('headerNavMenuCloseIcon');
   const headerNavMenuLinkList = document.querySelector('.header-nav__menu-link-list');
   const headerNavMenuLinkItems = document.querySelectorAll('.header-nav__menu-link-item');
-
+  
   const documentScroll = () => {
     header.classList.toggle('header--scroll', window.scrollY > 0);
     headerNav.classList.toggle('header-nav--scroll', window.scrollY > 0);
+  };
+
+  const toggleMenu = () => {
+    const body = document.querySelector('.body');
+    body.classList.toggle('body--light');
+    headerNavThemeIconContainer.classList.toggle('header-nav__theme-icon-container--active');
   };
 
   const openMenu = () => {
@@ -21,6 +29,7 @@ const header = () => {
   };
 
   document.addEventListener('scroll', documentScroll);
+  headerNavThemeIconContainer.addEventListener('click', toggleMenu);
   headerNavMenuIcon.addEventListener('click', openMenu);
   headerNavMenuCloseIcon.addEventListener('click', closeMenu);
   headerNavMenuLinkItems.forEach((element) => {
