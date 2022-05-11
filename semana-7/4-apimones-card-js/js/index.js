a137f1c0b73f11240e6c9d692ed4b90fa17c60a9
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-10 19:29:13 -0500

    s7.4: Estructura y recursos iniciales

--- semana-7/4-apimones-card-js/js/index.js
+++ semana-7/4-apimones-card-js/js/index.js
@@ -1,12 +1,10 @@
 'use strict';
-import card from './card.js';
 import header from './header.js';
 import loader from './loader.js';
 
 const documentReady = () => {
   loader();
   header();
-  card();
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
