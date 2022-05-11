1e4c2bd4c635c10f03b4061d3abe4b0a86038d62
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-10 19:39:19 -0500

    s7.4: JSON in JS

--- semana-7/4-apimones-card-js/js/index.js
+++ semana-7/4-apimones-card-js/js/index.js
@@ -5,6 +5,26 @@ import loader from './loader.js';
 const documentReady = () => {
   loader();
   header();
+
+  const objectPerson = {
+    name: 'Elliot',
+    goal: 'Ser un gran programador frontend',
+    married: false,
+    years: 26
+  };
+
+  const objectPersonJson = `{
+    "name": "Elliot",
+    "goal": "Ser un gran programador frontend",
+    "married": false,
+    "years": 26
+  }`;
+
+  console.log(objectPerson);
+  console.log(objectPersonJson);
+
+  console.log(JSON.stringify(objectPerson));
+  console.log(JSON.parse(objectPersonJson));
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
