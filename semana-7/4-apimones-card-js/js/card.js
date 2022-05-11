ec036a7ee62a7bfe414127e949f2487d862d73e1
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-10 23:05:55 -0500

    s7.4: js card fetchApi json data

--- semana-7/4-apimones-card-js/js/card.js
+++ semana-7/4-apimones-card-js/js/card.js
@@ -1,8 +1,21 @@
 import { renderPokemon } from "../utils/util.js";
 
 const card = () => {
-  const card = document.getElementById('card');
-  renderPokemon(card, 'pikachu', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png');
+  const fetchApi = async () => {
+    document.getElementById('spinner').classList.remove('spinner--hidden');
+    try {
+      const response = await fetch('http://127.0.0.1:5500/semana-7/4-apimones-card-js/json/data.json');
+      const data = await response.json();
+      const { name, src } = data;
+      const card = document.getElementById('card');
+      renderPokemon(card, name, src);
+    } catch (error) {
+      console.log(errro);
+    } finally {
+      document.getElementById('spinner').classList.add('spinner--hidden');
+    }
+  }
+  fetchApi();
 };
 
 export default card;
