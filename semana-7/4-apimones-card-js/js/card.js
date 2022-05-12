37b0d103169bcd0bc989294986f43c15c3c0abc6
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-11 19:43:59 -0500

    s7.4: examples

--- semana-7/4-apimones-card-js/js/card.js
+++ semana-7/4-apimones-card-js/js/card.js
@@ -1,4 +1,5 @@
 // import { fetchApi, renderCard } from "../utils/util.js";
+// import { renderPokemon } from "../utils/util.js";
 
 const card = async () => {
 
@@ -29,6 +30,22 @@ const card = async () => {
   // const pokemonPokemonRemote = await fetchApi('https://pokeapi.co/api/v2/pokemon/25');
   // renderCard(pokemonLocal, pokemonPokemonLocal);
   // renderCard(pokemonRemote, pokemonPokemonRemote);
+
+  // const fetchApi = async () => {
+  //   document.getElementById('spinner').classList.remove('spinner--hidden');
+  //   try {
+  //     const response = await fetch('http://127.0.0.1:5500/semana-7/4-apimones-card-js/json/data.json');
+  //     const data = await response.json();
+  //     const { name, src } = data;
+  //     const card = document.getElementById('card');
+  //     renderPokemon(card, name, src);
+  //   } catch (error) {
+  //     console.log(errro);
+  //   } finally {
+  //     document.getElementById('spinner').classList.add('spinner--hidden');
+  //   }
+  // }
+  // fetchApi();
 }
 
 export default card;
--- semana-7/4-apimones-card-js/utils/util.js
+++ semana-7/4-apimones-card-js/utils/util.js
@@ -82,3 +82,29 @@
 //     </div>
 //   `);
 // };
+
+// export const renderPokemon = (element, name, src) => {
+//   const fragment = document.createDocumentFragment();
+
+//   const cardPokemon = document.createElement('div');
+//   cardPokemon.classList.add('card__pokemon');
+
+//   const namePokemon = document.createElement('h3');
+//   namePokemon.textContent = name;
+//   namePokemon.classList.add('card__pokemon-name');
+
+//   const imageContainerPokemon = document.createElement('figure');
+//   imageContainerPokemon.classList.add('card__pokemon-image-container');
+
+//   const imagePokemon = document.createElement('img');
+//   imagePokemon.setAttribute('src', src);
+//   imagePokemon.setAttribute('alt', name);
+//   imagePokemon.classList.add('card__pokemon-image');
+
+//   cardPokemon.appendChild(namePokemon);
+//   imageContainerPokemon.appendChild(imagePokemon);
+//   cardPokemon.appendChild(imageContainerPokemon);
+//   fragment.appendChild(cardPokemon);
+
+//   element.appendChild(fragment);
+// };
