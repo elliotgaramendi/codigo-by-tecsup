61c40a382b689ac23056d7477bde829a456683be
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-13 20:53:35 -0500

    Instalación de animate.css

--- semana-8/1-sin-e-spoiler/src/App.js
+++ semana-8/1-sin-e-spoiler/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <>
-      <img src={logo} alt="logo" className="body__img"/>
-      <h1 className="body__title">Sin E Spoiler</h1>
+      <img src={logo} alt="logo" className="body__img animate__animated animate__fadeIn" />
+      <h1 className="body__title animate__animated animate__fadeIn">Sin E Spoiler</h1>
     </>
   );
 }
--- semana-8/1-sin-e-spoiler/src/index.js
+++ semana-8/1-sin-e-spoiler/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'animate.css/animate.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
