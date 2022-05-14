d0d3e88a9443c2c4af28f688548f506dcdcdf9e1
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-14 10:23:26 -0500

    Update component Header

--- semana-8/1-sin-e-spoiler/src/components/sections/Header.jsx
+++ semana-8/1-sin-e-spoiler/src/components/sections/Header.jsx
@@ -2,6 +2,23 @@ import logo from '../../assets/img/logo.png';
 import '../../styles/css/Header.css';
 
 const Header = () => {
+
+  const documentReady = () => {
+
+    const header = document.querySelector('.header');
+    const headerNav = document.querySelector('.header-nav');
+
+    const documentScroll = () => {
+      const { scrollY } = window;
+      header.classList.toggle('header--scroll', scrollY > 0);
+      headerNav.classList.toggle('header-nav--scroll', scrollY > 0);
+    };
+
+    document.addEventListener('scroll', documentScroll);
+  };
+
+  window.addEventListener('load', documentReady);
+
   return (
     <header className="header animate__animated animate__fadeIn">
       <nav className="header-nav">
@@ -35,7 +52,7 @@ const Header = () => {
           </div>
           <div className="header-nav__form-theme-menu-container">
             <form className="header-nav__form">
-              <input type="search" placeholder="Película" className="header-nav__form-input" required/>
+              <input type="search" placeholder="Película" className="header-nav__form-input" required />
             </form>
             <button className="header-nav__theme-icon-container" id="headerNavThemeIconContainer">
               <span className="header-nav__theme-icon">🌚</span>
