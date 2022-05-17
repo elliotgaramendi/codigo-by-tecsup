bdeeddf900de9292afbdd737b091bfd7c23fc422
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-16 22:29:16 -0500

    Desinstalación de axios

--- semana-8/1-sin-e-spoiler/src/pages/Movies.jsx
+++ semana-8/1-sin-e-spoiler/src/pages/Movies.jsx
@@ -1,4 +1,3 @@
-// import axios from 'axios';
 import { useEffect } from 'react';
 import '../styles/css/Movies.css';
 
@@ -6,27 +5,17 @@ const Movies = () => {
   useEffect(() => {
     const fetchApi = async () => {
       const endpoint = `${process.env.REACT_APP_URL}/discover/movie`;
-      // const options = {
-      //   method: 'GET',
-      //   headers: {
-      //     'Authorization': `Bearer ${process.env.REACT_APP_API_TOKEN}`,
-      //     'Content-Type': 'application/json;charset=utf-8'
-      //   }
-      // };
       const options = {
         method: 'GET',
         headers: {
           'Authorization': `Bearer ${process.env.REACT_APP_API_TOKEN}`,
           'Content-Type': 'application/json;charset=utf-8'
-        },
-        url: endpoint
+        }
       };
       try {
-        // const response = await fetch(endpoint, options);
-        // const data = await response.json();
-        // console.log(data);
-        const response = await axios(options);
-        console.log(response.data);
+        const response = await fetch(endpoint, options);
+        const data = await response.json();
+        console.log(data);
       } catch (error) {
         console.log(error);
       }
