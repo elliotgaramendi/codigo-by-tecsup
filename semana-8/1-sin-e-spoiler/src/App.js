36bc126c5ddb625251d0fdaad3b29cab36a83813
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-16 20:36:39 -0500

    page Movies

--- semana-8/1-sin-e-spoiler/src/App.js
+++ semana-8/1-sin-e-spoiler/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Header from './components/sections/Header';
+import Movies from './pages/Movies';
 
 function App() {
   return (
     <>
       <Header />
-      <main className="main">
-        <h1 className="body__title animate__animated animate__fadeIn">Sin E Spoiler</h1>
-      </main>
+      <Movies />
     </>
   );
 }
---
+++ semana-8/1-sin-e-spoiler/src/pages/Movies.jsx
@@ -0,0 +1,15 @@
+import '../styles/css/Movies.css';
+
+const Movies = () => {
+  return (
+    <main className="main">
+      <section className="movies">
+        <div className="movies__container">
+          <h1 className="movies__title">🎥 Movies 🎥</h1>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Movies;
