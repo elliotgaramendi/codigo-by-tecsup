13071513b6497299b58302222a1b6b8c23433f66
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-16 21:14:18 -0500

    s8.1: Variables de entorno

--- semana-8/1-sin-e-spoiler/src/pages/Movies.jsx
+++ semana-8/1-sin-e-spoiler/src/pages/Movies.jsx
@@ -1,6 +1,10 @@
 import '../styles/css/Movies.css';
 
 const Movies = () => {
+
+  console.log(process.env.REACT_APP_URL);
+  console.log(process.env.REACT_APP_API_TOKEN);
+
   return (
     <main className="main">
       <section className="movies">
