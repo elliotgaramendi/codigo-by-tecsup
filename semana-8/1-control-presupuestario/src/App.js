1fdad92f21be5917083312dcb387065cbbfd3b8d
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-17 20:21:57 -0500

    s8.1: Estilos iniciales del proyecto

--- semana-8/1-control-presupuestario/src/App.js
+++ semana-8/1-control-presupuestario/src/App.js
@@ -3,22 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Control Presupuestario
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Control Presupuestario</h1>
+    </>
   );
 }
 
