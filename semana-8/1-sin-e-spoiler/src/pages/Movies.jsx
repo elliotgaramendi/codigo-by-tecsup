1232b1d4589020ea08a5384b2818fadbd0a85fc5
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-17 19:47:37 -0500

    component MoviesCard

--- semana-8/1-sin-e-spoiler/src/pages/Movies.jsx
+++ semana-8/1-sin-e-spoiler/src/pages/Movies.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import MoviesCard from '../components/movies/MoviesCard';
 import '../styles/css/Movies.css';
 
 const Movies = () => {
@@ -32,23 +33,12 @@ const Movies = () => {
           <h1 className="movies__title">🎥 Movies 🎥</h1>
           <ul className="movies__movie-list">
             {
-              data.results?.map((element) => {
-                const { id, poster_path, title } = element;
-                const imageEndpoint = `https://image.tmdb.org/t/p/w300${poster_path}`;
+              data.results?.map((movie) => {
                 return (
-                  <li
-                    key={id}
-                    className="movies__movie-item"
-                  >
-                    <figure className="movies__movie-poster-container">
-                      <img
-                        src={imageEndpoint}
-                        alt={title}
-                        className="movies__movie-poster"
-                      />
-                    </figure>
-                    <h2 className="movies__movie-title">{title}</h2>
-                  </li>
+                  <MoviesCard
+                    key={movie.id}
+                    movie={movie}
+                  />
                 );
               })
             }
