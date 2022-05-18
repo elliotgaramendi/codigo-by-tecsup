1318ded38060a9bae1ffb8af23b15e65580c7924
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-17 20:36:52 -0500

    s8.1: component Header

--- semana-8/1-control-presupuestario/src/App.js
+++ semana-8/1-control-presupuestario/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <img src={logo} className="App-logo" alt="logo" />
-      <h1>Control Presupuestario</h1>
     </>
   );
 }
---
+++ semana-8/1-control-presupuestario/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Control Presupuestario</h1>
+    </header>
+  );
+};
+
+export default Header;
