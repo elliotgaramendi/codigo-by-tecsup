9352c4468903930b547ef55b3b0f8c5e859ae88f
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-20 21:06:46 -0500

    s8.3: Update component AppointmentsForm

--- semana-8/3-patita-feliz/src/components/appointments/AppointmentsForm.jsx
+++ semana-8/3-patita-feliz/src/components/appointments/AppointmentsForm.jsx
@@ -1,38 +1,103 @@
+import { useState } from "react";
+
 const AppointmentsForm = () => {
+
+  const [formAppointment, setFormAppointment] = useState({
+    mascota: '',
+    propietario: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+  });
+
+  const handleChange = (e) => {
+    setFormAppointment({
+      ...formAppointment,
+      [e.target.name]: e.target.value
+    });
+  };
+
   return (
     <section className="col-md-4">
       <h3 className="text-center" id="formTitle">🐱Generar cita🐱</h3>
       <form className="text-dark rounded p-4" id="formPokemon">
         <div className="text-dark form-floating mb-3" style={{ display: "none" }}>
-          <input type="number" className="form-control" name="id" placeholder="Id" id="formId" readOnly />
+          <input
+            type="number"
+            className="form-control"
+            name="id"
+            placeholder="Id"
+            id="formId"
+            readOnly
+          />
           <label htmlFor="id" className="text-dark">Id</label>
         </div>
 
         <div className="form-floating mb-3">
-          <input type="text" className="form-control" name="mascota" placeholder="Nombre de la Mascota" required />
+          <input
+            type="text"
+            name="mascota"
+            placeholder="Nombre de la Mascota"
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
           <label htmlFor="mascota" className="text-dark">Nombre de la Mascota</label>
         </div>
 
         <div className="form-floating mb-3">
-          <input type="text" className="form-control" name="propietario" placeholder="Nombre del propietario" required />
+          <input
+            type="text"
+            name="propietario"
+            placeholder="Nombre del propietario"
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
           <label htmlFor="propietario" className="text-dark">Nombre del propietario</label>
         </div>
 
         <div className="form-floating mb-3">
-          <input type="date" className="form-control" name="fecha" placeholder="Fecha" required />
+          <input
+            type="date"
+            name="fecha"
+            placeholder="Fecha"
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
           <label htmlFor="fecha" className="text-dark">Fecha</label>
         </div>
 
         <div className="form-floating mb-3">
-          <input type="time" className="form-control" name="hora" placeholder="Hora" required />
+          <input
+            type="time"
+            name="hora"
+            placeholder="Hora"
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
           <label htmlFor="hora" className="text-dark">Hora</label>
         </div>
 
         <div className="form-floating mb-3">
-          <input type="text" className="form-control" name="sintomas" placeholder="Síntomas" required />
+          <input
+            type="text"
+            name="sintomas"
+            placeholder="Síntomas"
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
           <label htmlFor="sintomas" className="text-dark">Síntomas</label>
         </div>
-        <button type="submit" className="btn btn-primary w-100" id="formButton">Crear</button>
+        <button
+          type="submit"
+          className="btn btn-primary w-100"
+        >
+          Crear
+        </button>
       </form>
       <div className="badge w-100" id="contenedorAlerta"></div>
     </section>
