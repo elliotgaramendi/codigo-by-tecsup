c776c3d22b814313147bda3dcc15259b90fa4f14
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-25 22:29:10 -0500

    s9.1: component Header

--- semana-9/1-pokemon-inc-mern-frontend/src/layouts/PokemonsLayout.jsx
+++ semana-9/1-pokemon-inc-mern-frontend/src/layouts/PokemonsLayout.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/sections/Header";
 
 const PokemonsLayout = () => {
+
+  const applicationName = "PokémonInc";
+
   return (
     <>
-      <header className="text-center">
-        PokémonInc
-      </header>
-      <main className="text-center">
+      <Header
+        applicationName={applicationName}
+      />
+      <main className="text-center pt-16 pb-8">
         <Outlet />
       </main>
       <footer className="mt-auto text-center">
