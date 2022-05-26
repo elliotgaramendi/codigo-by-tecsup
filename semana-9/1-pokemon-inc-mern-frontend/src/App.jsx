4fb1f0109a53c2e5c06e9cd0c596642d1bb73acf
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-25 20:23:35 -0500

    s9.1: Configuración de react-router-dom

--- semana-9/1-pokemon-inc-mern-frontend/src/App.jsx
+++ semana-9/1-pokemon-inc-mern-frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import {
+  Routes,
+  Route
+} from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <h1>Pokémon Inc MERN</h1>
+    <Routes>
+      <Route path="/create" element={<h2>Create Pokémons</h2>} />
+      <Route path="/update" element={<h2>Update Pokémons</h2>} />
+      <Route path="/" element={<h2>Pokémons</h2>} />
+      <Route path="*" element={<h2>404</h2>} />
+    </Routes>
   );
 }
 
