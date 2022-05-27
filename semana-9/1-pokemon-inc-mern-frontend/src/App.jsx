104d8fab587d9ef9d6753f519a9d5b6ec22d864e
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-26 20:04:17 -0500

    s9.1: Update src App.jsx PokemonsProvider

--- semana-9/1-pokemon-inc-mern-frontend/src/App.jsx
+++ semana-9/1-pokemon-inc-mern-frontend/src/App.jsx
@@ -5,19 +5,22 @@ import {
 } from 'react-router-dom';
 
 import './App.css';
+import { PokemonsProvider } from './context/PokemonsContext';
 import PokemonsLayout from './layouts/PokemonsLayout';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/pokemons" replace={true} />} />
-      <Route path="/pokemons" element={<PokemonsLayout />}>
-        <Route path="create" element={<h2>Create Pokémons</h2>} />
-        <Route path="update" element={<h2>Update Pokémons</h2>} />
-        <Route index element={<h2>Pokémons</h2>} />
-      </Route>
-      <Route path="*" element={<h2>404</h2>} />
-    </Routes>
+    <PokemonsProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/pokemons" replace={true} />} />
+        <Route path="/pokemons" element={<PokemonsLayout />}>
+          <Route path="create" element={<h2>Create Pokémons</h2>} />
+          <Route path="update" element={<h2>Update Pokémons</h2>} />
+          <Route index element={<h2>Pokémons</h2>} />
+        </Route>
+        <Route path="*" element={<h2>404</h2>} />
+      </Routes>
+    </PokemonsProvider>
   );
 }
 
