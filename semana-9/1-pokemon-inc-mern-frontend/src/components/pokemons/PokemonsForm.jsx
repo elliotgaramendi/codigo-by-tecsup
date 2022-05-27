b4410dc08f402f492a2c11d02aa7bd9f6f0ad8d8
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-26 22:31:18 -0500

    s9.1: Update component PokemonsForm yup

--- semana-9/1-pokemon-inc-mern-frontend/src/components/pokemons/PokemonsForm.jsx
+++ semana-9/1-pokemon-inc-mern-frontend/src/components/pokemons/PokemonsForm.jsx
@@ -1,4 +1,5 @@
-import { Field, Form, Formik } from 'formik';
+import * as yup from 'yup';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 
 const PokemonsForm = () => {
   return (
@@ -11,6 +12,13 @@ const PokemonsForm = () => {
         special: '',
         image: {}
       }}
+      validationSchema={yup.object({
+        name: yup.string().required('El nombre es requerido'),
+        type: yup.string().required('El tipo es requerido'),
+        hp: yup.number().required('El Hp es requerido').positive().integer(),
+        attack: yup.string().required('El ataque es requerido'),
+        special: yup.string().required('El especial es requerido')
+      })}
       onSubmit={(values) => {
         console.log(values);
       }}
@@ -21,11 +29,26 @@ const PokemonsForm = () => {
             onSubmit={handleSubmit}
             className="flex flex-col gap-4 bg-zinc-700 rounded-md w-4/5 max-w-sm mx-auto p-4"
           >
-            <Field name="name" type="text" placeholder="Nombre" className="bg-zinc-800 rounded p-2" />
-            <Field name="type" type="text" placeholder="Tipo" className="bg-zinc-800 rounded p-2" />
-            <Field name="hp" type="number" placeholder="Hp" className="bg-zinc-800 rounded p-2" />
-            <Field name="attack" type="text" placeholder="Ataque" className="bg-zinc-800 rounded p-2" />
-            <Field name="special" type="text" placeholder="Especial" className="bg-zinc-800 rounded p-2" />
+            <div className="flex flex-col gap-1">
+              <Field name="name" type="text" placeholder="Nombre" className="bg-zinc-800 rounded p-2" />
+              <ErrorMessage name="name" component="span" className="text-rose-500 text-sm text-center" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Field name="type" type="text" placeholder="Tipo" className="bg-zinc-800 rounded p-2" />
+              <ErrorMessage name="type" component="span" className="text-rose-500 text-sm text-center" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Field name="hp" type="number" placeholder="Hp" className="bg-zinc-800 rounded p-2" />
+              <ErrorMessage name="hp" component="span" className="text-rose-500 text-sm text-center" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Field name="attack" type="text" placeholder="Ataque" className="bg-zinc-800 rounded p-2" />
+              <ErrorMessage name="attack" component="span" className="text-rose-500 text-sm text-center" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Field name="special" type="text" placeholder="Especial" className="bg-zinc-800 rounded p-2" />
+              <ErrorMessage name="special" component="span" className="text-rose-500 text-sm text-center" />
+            </div>
             <input type="file" className="bg-zinc-800 rounded p-2 text-sm cursor-pointer file:bg-white file:border-0 file:rounded file:font-semibold file:mr-2 file:p-1 file:px-2 file:cursor-pointer file:transition-colors hover:file:bg-zinc-300" />
             <button type="submit" className="bg-cyan-300 rounded-md text-black font-semibold p-2 transition-colors hover:bg-white">Enviar</button>
           </Form>
