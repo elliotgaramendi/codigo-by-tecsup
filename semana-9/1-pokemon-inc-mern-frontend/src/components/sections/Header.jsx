23de863e4a5adde904f4635ef8aae89d6226d24d
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-26 19:43:13 -0500

    s9.1: Update component Header

--- semana-9/1-pokemon-inc-mern-frontend/src/components/sections/Header.jsx
+++ semana-9/1-pokemon-inc-mern-frontend/src/components/sections/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({ applicationName }) => {
       <nav className="py-4">
         <div className="flex items-center justify-between container mx-auto px-8 md:px-4">
           <Link
-            to="/pokemons"
+            to=""
             className="text-cyan-300 text-2xl font-black transition-colors hover:text-white"
           >
             {applicationName}
@@ -15,9 +15,9 @@ const Header = ({ applicationName }) => {
             <li className="flex">
               <NavLink
                 to="create"
-                className={({ isActive }) =>
-                  isActive ? "bg-cyan-300 text-black rounded-xl text-xl font-bold px-2 transition-colors" : "rounded-xl text-xl font-bold px-2 transition-colors hover:bg-cyan-300 hover:text-black"
-                }
+                className={({ isActive }) => {
+                  return isActive ? "bg-cyan-300 rounded-xl text-black text-xl font-bold px-2" : "rounded-xl text-white text-xl font-bold px-2 transition-colors hover:bg-cyan-300 hover:text-black";
+                }}
               >
                 Crear
               </NavLink>
