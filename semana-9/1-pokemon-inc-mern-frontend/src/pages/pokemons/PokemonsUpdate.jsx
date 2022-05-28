e5a9fe6ed57d459b4591c02e8e73c88a5a0a479b
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-28 09:53:01 -0500

    s9.1: Update page PokemonsUpdate

--- semana-9/1-pokemon-inc-mern-frontend/src/pages/pokemons/PokemonsUpdate.jsx
+++ semana-9/1-pokemon-inc-mern-frontend/src/pages/pokemons/PokemonsUpdate.jsx
@@ -1,6 +1,18 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 import PokemonsForm from "../../components/pokemons/PokemonsForm";
+import usePokemons from "../../hooks/usePokemons";
 
 const PokemonsUpdate = () => {
+
+  const { id } = useParams();
+  const { readPokemon } = usePokemons();
+
+  useEffect(() => {
+    readPokemon(id);
+    // eslint-disable-next-line
+  }, [id]);
+
   return (
     <section className="py-8">
       <div className="flex flex-col gap-8 container mx-auto px-8 md:px-4">
