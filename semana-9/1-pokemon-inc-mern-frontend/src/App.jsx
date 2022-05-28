1014531c75e6d979e7be85e78f6eece9edc636ab
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-27 22:00:55 -0500

    s9.1: page PokemonsRead

--- semana-9/1-pokemon-inc-mern-frontend/src/App.jsx
+++ semana-9/1-pokemon-inc-mern-frontend/src/App.jsx
@@ -8,6 +8,7 @@ import './App.css';
 import { PokemonsProvider } from './context/PokemonsContext';
 import PokemonsLayout from './layouts/PokemonsLayout';
 import PokemonsCreate from './pages/pokemons/PokemonsCreate';
+import PokemonsRead from './pages/pokemons/PokemonsRead';
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
         <Route path="/pokemons" element={<PokemonsLayout />}>
           <Route path="create" element={<PokemonsCreate />} />
           <Route path="update" element={<h2>Update Pokémons</h2>} />
-          <Route index element={<h2>Pokémons</h2>} />
+          <Route index element={<PokemonsRead />} />
         </Route>
         <Route path="*" element={<h2>404</h2>} />
       </Routes>
---
+++ semana-9/1-pokemon-inc-mern-frontend/src/pages/pokemons/PokemonsRead.jsx
@@ -0,0 +1,16 @@
+import usePokemons from "../../hooks/usePokemons";
+
+const PokemonsRead = () => {
+
+  const { pokemons } = usePokemons();
+
+  return (
+    <section className="py-8">
+      <div className="flex flex-col gap-8 container mx-auto px-8 md:px-4">
+        <h2 className="text-yellow-400 text-3xl text-center font-bold">Pokémons {pokemons.length}</h2>
+      </div>
+    </section>
+  );
+};
+
+export default PokemonsRead;
