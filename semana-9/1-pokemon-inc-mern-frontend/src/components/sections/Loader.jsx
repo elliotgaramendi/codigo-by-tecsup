c566a531e472700c7db88c6fda077ef929d24df6
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-30 21:55:54 -0500

    s9.1: component Loader

---
+++ semana-9/1-pokemon-inc-mern-frontend/src/components/sections/Loader.jsx
@@ -0,0 +1,18 @@
+const Loader = () => {
+  return (
+    <section class="loader loader--sm loader--transparent" id="loader">
+      <div class="loader__wave loader__wave--sm"></div>
+      <div class="loader__wave loader__wave--sm"></div>
+      <div class="loader__wave loader__wave--sm"></div>
+      <div class="loader__wave loader__wave--sm"></div>
+      <div class="loader__wave loader__wave--sm"></div>
+      <div class="loader__wave loader__wave--sm"></div>
+      <div class="loader__wave loader__wave--sm"></div>
+      <div class="loader__wave loader__wave--sm"></div>
+      <div class="loader__wave loader__wave--sm"></div>
+      <div class="loader__wave loader__wave--sm"></div>
+    </section>
+  );
+}
+
+export default Loader;
