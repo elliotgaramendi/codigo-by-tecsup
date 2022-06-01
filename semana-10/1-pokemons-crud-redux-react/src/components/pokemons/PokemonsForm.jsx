78117725f513e4130cfd09b722bc0285af33cc73
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-31 23:06:46 -0500

    s10.1: Update component PokemonsForm yup

--- semana-10/1-pokemons-crud-redux-react/src/components/pokemons/PokemonsForm.jsx
+++ semana-10/1-pokemons-crud-redux-react/src/components/pokemons/PokemonsForm.jsx
@@ -1,4 +1,5 @@
-import { Field, Form, Formik } from 'formik';
+import * as yup from 'yup';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 
 const PokemonsForm = () => {
   return (
@@ -11,6 +12,13 @@ const PokemonsForm = () => {
         special: '',
         image: {}
       }}
+      validationSchema={yup.object({
+        name: yup.string().required('El nombre es requerido'),
+        type: yup.string().required('El tipo es requerido'),
+        hp: yup.number().required('El Hp es requerido').positive().integer(),
+        attack: yup.string().required('El ataque es requerido'),
+        special: yup.string().required('El especial es requerido')
+      })}
       enableReinitialize
       onSubmit={async (values, actions) => {
         console.log(values);
@@ -24,11 +32,26 @@ const PokemonsForm = () => {
             onSubmit={handleSubmit}
             className="flex flex-col gap-4 bg-zinc-700 rounded-md w-4/5 max-w-sm mx-auto p-4"
           >
-            <Field name="name" type="text" placeholder="Nombre" className="bg-zinc-800 rounded p-2" required />
-            <Field name="type" type="text" placeholder="Tipo" className="bg-zinc-800 rounded p-2" required />
-            <Field name="hp" type="number" placeholder="Hp" className="bg-zinc-800 rounded p-2" required />
-            <Field name="attack" type="text" placeholder="Ataque" className="bg-zinc-800 rounded p-2" required />
-            <Field name="special" type="text" placeholder="Especial" className="bg-zinc-800 rounded p-2" required />
+            <div className="flex flex-col gap-1">
+              <Field name="name" type="text" placeholder="Nombre" className="bg-zinc-800 rounded p-2" required />
+              <ErrorMessage name="name" component="span" className="text-rose-500 text-sm text-center" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Field name="type" type="text" placeholder="Tipo" className="bg-zinc-800 rounded p-2" required />
+              <ErrorMessage name="type" component="span" className="text-rose-500 text-sm text-center" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Field name="hp" type="number" placeholder="Hp" className="bg-zinc-800 rounded p-2" required />
+              <ErrorMessage name="hp" component="span" className="text-rose-500 text-sm text-center" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Field name="attack" type="text" placeholder="Ataque" className="bg-zinc-800 rounded p-2" required />
+              <ErrorMessage name="attack" component="span" className="text-rose-500 text-sm text-center" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Field name="special" type="text" placeholder="Especial" className="bg-zinc-800 rounded p-2" required />
+              <ErrorMessage name="special" component="span" className="text-rose-500 text-sm text-center" />
+            </div>
             <input
               type="file"
               name="image"
