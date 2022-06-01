efc8ecf66dd4f93c406cafcf29eeae769e8a1400
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-31 20:43:53 -0500

    Configuración de react-router-dom

--- semana-10/1-pokemons-crud-redux-react/src/App.js
+++ semana-10/1-pokemons-crud-redux-react/src/App.js
@@ -1,8 +1,22 @@
+import {
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <h1>Pokémons CRUD Redux React</h1>
+    <Routes>
+      <Route path="/" element={<Navigate to="/pokemons" replace={true} />} />
+      <Route path="/pokemons">
+        <Route path="create" element={<h2>Crear Pokémon</h2>} />
+        <Route path=":id" element={<h2>Actualizar Pokémon</h2>} />
+        <Route index element={<h2>Pokémons</h2>} />
+      </Route>
+      <Route path="*" element={<h2>404</h2>} />
+    </Routes>
   );
 }
 
