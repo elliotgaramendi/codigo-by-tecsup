c37379c64fb58fe07541b69d3da72c1406c39139
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-31 23:10:16 -0500

    s9.1: Update component PokemonsForm

--- semana-9/1-pokemon-inc-mern-frontend/src/components/pokemons/PokemonsForm.jsx
+++ semana-9/1-pokemon-inc-mern-frontend/src/components/pokemons/PokemonsForm.jsx
@@ -46,23 +46,23 @@ const PokemonsForm = () => {
             className="flex flex-col gap-4 bg-zinc-700 rounded-md w-4/5 max-w-sm mx-auto p-4"
           >
             <div className="flex flex-col gap-1">
-              <Field name="name" type="text" placeholder="Nombre" className="bg-zinc-800 rounded p-2" />
+              <Field name="name" type="text" placeholder="Nombre" className="bg-zinc-800 rounded p-2" required />
               <ErrorMessage name="name" component="span" className="text-rose-500 text-sm text-center" />
             </div>
             <div className="flex flex-col gap-1">
-              <Field name="type" type="text" placeholder="Tipo" className="bg-zinc-800 rounded p-2" />
+              <Field name="type" type="text" placeholder="Tipo" className="bg-zinc-800 rounded p-2" required />
               <ErrorMessage name="type" component="span" className="text-rose-500 text-sm text-center" />
             </div>
             <div className="flex flex-col gap-1">
-              <Field name="hp" type="number" placeholder="Hp" className="bg-zinc-800 rounded p-2" />
+              <Field name="hp" type="number" placeholder="Hp" className="bg-zinc-800 rounded p-2" required />
               <ErrorMessage name="hp" component="span" className="text-rose-500 text-sm text-center" />
             </div>
             <div className="flex flex-col gap-1">
-              <Field name="attack" type="text" placeholder="Ataque" className="bg-zinc-800 rounded p-2" />
+              <Field name="attack" type="text" placeholder="Ataque" className="bg-zinc-800 rounded p-2" required />
               <ErrorMessage name="attack" component="span" className="text-rose-500 text-sm text-center" />
             </div>
             <div className="flex flex-col gap-1">
-              <Field name="special" type="text" placeholder="Especial" className="bg-zinc-800 rounded p-2" />
+              <Field name="special" type="text" placeholder="Especial" className="bg-zinc-800 rounded p-2" required />
               <ErrorMessage name="special" component="span" className="text-rose-500 text-sm text-center" />
             </div>
             <input
