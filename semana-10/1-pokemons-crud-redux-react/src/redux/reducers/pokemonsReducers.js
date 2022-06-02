180632001dba27d30eab3109f94a0fe449956f13
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-01 20:31:45 -0500

    10.1: Update redux pokemonsReducers FETCH_CREATE_POKEMON_REQUEST

--- semana-10/1-pokemons-crud-redux-react/src/redux/reducers/pokemonsReducers.js
+++ semana-10/1-pokemons-crud-redux-react/src/redux/reducers/pokemonsReducers.js
@@ -1,9 +1,17 @@
+import { FETCH_CREATE_POKEMON_REQUEST } from "../types/pokemonsTypes";
+
 const initialState = {
   loading: false
 };
 
 const pokemonsReducers = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_CREATE_POKEMON_REQUEST:
+      console.log(action.payload);
+      return {
+        ...state,
+        loading: true
+      };
     default:
       return state;
   }
