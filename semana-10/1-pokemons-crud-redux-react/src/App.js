aba536d9a094eeb403005a0cae0fae845fcef7b9
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-01 19:57:42 -0500

    10.1: Configuración de react-redux & Update src App.js

--- semana-10/1-pokemons-crud-redux-react/src/App.js
+++ semana-10/1-pokemons-crud-redux-react/src/App.js
@@ -1,3 +1,4 @@
+import { Provider } from 'react-redux';
 import {
   Routes,
   Route,
@@ -9,17 +10,18 @@ import PokemonsCreate from './pages/pokemons/PokemonsCreate';
 import store from './redux/store';
 
 function App() {
-  console.log(store.getState());
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/pokemons" replace={true} />} />
-      <Route path="/pokemons" element={<PokemonsLayout />}>
-        <Route path="create" element={<PokemonsCreate />} />
-        <Route path=":id" element={<h2>Actualizar Pokémon</h2>} />
-        <Route index element={<h2>Pokémons</h2>} />
-      </Route>
-      <Route path="*" element={<h2>404</h2>} />
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<Navigate to="/pokemons" replace={true} />} />
+        <Route path="/pokemons" element={<PokemonsLayout />}>
+          <Route path="create" element={<PokemonsCreate />} />
+          <Route path=":id" element={<h2>Actualizar Pokémon</h2>} />
+          <Route index element={<h2>Pokémons</h2>} />
+        </Route>
+        <Route path="*" element={<h2>404</h2>} />
+      </Routes>
+    </Provider>
   );
 }
 
