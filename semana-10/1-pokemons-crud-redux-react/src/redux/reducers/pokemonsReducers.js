59b93aacfdb3e3162d426956086443d99d36e8b1
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-02 22:58:14 -0500

    s10.1: Update redux pokemonsReducers FETCH_UPDATE_POKEMON

--- semana-10/1-pokemons-crud-redux-react/src/redux/reducers/pokemonsReducers.js
+++ semana-10/1-pokemons-crud-redux-react/src/redux/reducers/pokemonsReducers.js
@@ -7,7 +7,10 @@ import {
   FETCH_READ_POKEMONS_SUCCESS,
   FETCH_READ_POKEMON_ERROR,
   FETCH_READ_POKEMON_REQUEST,
-  FETCH_READ_POKEMON_SUCCESS
+  FETCH_READ_POKEMON_SUCCESS,
+  FETCH_UPDATE_POKEMON_ERROR,
+  FETCH_UPDATE_POKEMON_REQUEST,
+  FETCH_UPDATE_POKEMON_SUCCESS
 } from "../types/pokemonsTypes";
 
 const initialState = {
@@ -68,6 +71,25 @@ const pokemonsReducers = (state = initialState, action) => {
         pokemon: action.payload
       };
     case FETCH_READ_POKEMON_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+    case FETCH_UPDATE_POKEMON_REQUEST:
+      return {
+        ...state,
+        loading: action.payload
+      };
+    case FETCH_UPDATE_POKEMON_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: {},
+        pokemon: {},
+        pokemons: state.pokemons.map(pokemon => pokemon._id === action.payload._id ? action.payload : pokemon)
+      };
+    case FETCH_UPDATE_POKEMON_ERROR:
       return {
         ...state,
         loading: false,
