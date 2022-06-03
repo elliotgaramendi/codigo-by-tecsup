e4c0e57a454ed185d5bf3dac0cfef9649b22ba21
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-02 20:43:39 -0500

    s10.1: page PokemonsRead FETCH_READ_POKEMONS

--- semana-10/1-pokemons-crud-redux-react/src/App.js
+++ semana-10/1-pokemons-crud-redux-react/src/App.js
@@ -7,6 +7,7 @@ import {
 import './App.css';
 import PokemonsLayout from './layouts/Pokemonslayout';
 import PokemonsCreate from './pages/pokemons/PokemonsCreate';
+import PokemonsRead from './pages/pokemons/PokemonsRead';
 import store from './redux/store';
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
         <Route path="/pokemons" element={<PokemonsLayout />}>
           <Route path="create" element={<PokemonsCreate />} />
           <Route path=":id" element={<h2>Actualizar Pokémon</h2>} />
-          <Route index element={<h2>Pokémons</h2>} />
+          <Route index element={<PokemonsRead />} />
         </Route>
         <Route path="*" element={<h2>404</h2>} />
       </Routes>
