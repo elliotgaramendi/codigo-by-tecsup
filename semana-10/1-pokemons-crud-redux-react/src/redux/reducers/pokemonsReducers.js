1e868bb15c266dcba99d8a663fec335b84160ae8
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-02 20:27:34 -0500

    s10.1: Update redux pokemonsReducers FETCH_READ_POKEMONS

--- semana-10/1-pokemons-crud-redux-react/src/redux/reducers/pokemonsReducers.js
+++ semana-10/1-pokemons-crud-redux-react/src/redux/reducers/pokemonsReducers.js
@@ -1,7 +1,10 @@
 import {
   FETCH_CREATE_POKEMON_ERROR,
   FETCH_CREATE_POKEMON_REQUEST,
-  FETCH_CREATE_POKEMON_SUCCESS
+  FETCH_CREATE_POKEMON_SUCCESS,
+  FETCH_READ_POKEMONS_ERROR,
+  FETCH_READ_POKEMONS_REQUEST,
+  FETCH_READ_POKEMONS_SUCCESS
 } from "../types/pokemonsTypes";
 
 const initialState = {
@@ -25,6 +28,24 @@ const pokemonsReducers = (state = initialState, action) => {
         pokemons: [...state.pokemons, action.payload]
       };
     case FETCH_CREATE_POKEMON_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+    case FETCH_READ_POKEMONS_REQUEST:
+      return {
+        ...state,
+        loading: action.payload
+      };
+    case FETCH_READ_POKEMONS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: {},
+        pokemons: action.payload
+      };
+    case FETCH_READ_POKEMONS_ERROR:
       return {
         ...state,
         loading: false,
