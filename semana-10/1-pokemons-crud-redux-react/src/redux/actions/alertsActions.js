0bb803f9109158a7a7f77a4739522a0e2c9b6046
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-03 21:54:20 -0500

    s10.1: redux alertsActions RESET_ALERT

--- semana-10/1-pokemons-crud-redux-react/src/redux/actions/alertsActions.js
+++ semana-10/1-pokemons-crud-redux-react/src/redux/actions/alertsActions.js
@@ -1,8 +1,15 @@
-import { ADD_ALERT } from "../types/alertsTypes";
+import { ADD_ALERT, RESET_ALERT } from "../types/alertsTypes";
 
 export const addAlert = (alert) => {
   return ({
     type: ADD_ALERT,
+    payload: alert
+  });
+};
+
+export const resetAlert = (alert) => {
+  return ({
+    type: RESET_ALERT,
     payload: alert
   });
 };
