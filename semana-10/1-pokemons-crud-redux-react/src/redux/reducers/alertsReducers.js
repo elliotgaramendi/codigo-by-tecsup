5be72583513d646572402001d423ab288d1ce7b5
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-03 21:55:34 -0500

    s10.1: redux alertsReducers RESET_ALERT

--- semana-10/1-pokemons-crud-redux-react/src/redux/reducers/alertsReducers.js
+++ semana-10/1-pokemons-crud-redux-react/src/redux/reducers/alertsReducers.js
@@ -1,4 +1,4 @@
-import { ADD_ALERT } from "../types/alertsTypes";
+import { ADD_ALERT, RESET_ALERT } from "../types/alertsTypes";
 
 const initialState = {
   alert: {}
@@ -7,6 +7,11 @@ const initialState = {
 const alertsReducers = (state = initialState, action) => {
   switch (action.type) {
     case ADD_ALERT:
+      return {
+        ...state,
+        alert: action.payload
+      };
+    case RESET_ALERT:
       return {
         ...state,
         alert: action.payload
