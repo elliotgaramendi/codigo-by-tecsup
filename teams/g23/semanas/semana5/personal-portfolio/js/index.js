27d9d6843b0d5c4d47ee81259e10f9d636811473
elliotgaramendi <elliotgaramendi@gmail.com>
2023-09-27 22:27:17 -0500

    feat: update G23-5-1 header button to top

--- teams/g23/semanas/semana5/personal-portfolio/js/index.js
+++ teams/g23/semanas/semana5/personal-portfolio/js/index.js
@@ -3,9 +3,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('.header');
   const nav = document.querySelector('.nav');
+  const buttonToTop = document.getElementById('buttonToTop');
 
   document.addEventListener('scroll', () => {
     header.classList.toggle('header--scroll', window.scrollY > 0);
     nav.classList.toggle('nav--scroll', window.scrollY > 0);
+    buttonToTop.classList.toggle('button--show', window.scrollY > 0);
+  });
+
+  buttonToTop.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
   });
 });
