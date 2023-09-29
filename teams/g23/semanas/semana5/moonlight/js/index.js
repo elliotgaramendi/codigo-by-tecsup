2009dca76574a7ac2493ffee9f9afbd50bd99350
elliotgaramendi <elliotgaramendi@gmail.com>
2023-09-28 21:11:01 -0500

    feat: update G23-5-2 hero parallax effect

--- teams/g23/semanas/semana5/moonlight/js/index.js
+++ teams/g23/semanas/semana5/moonlight/js/index.js
@@ -1,6 +1,8 @@
 'use strict';
 import header from './modules/header.js';
+import hero from './modules/hero.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   header();
+  hero();
 });
---
+++ teams/g23/semanas/semana5/moonlight/js/modules/hero.js
@@ -0,0 +1,19 @@
+const hero = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroLearnButton = document.querySelector('.hero__learn-button');
+  const heroStars = document.querySelector('.hero__stars-image');
+  const heroMoon = document.querySelector('.hero__moon-image');
+  const heroMountainsBehind = document.querySelector('.hero__mountains-behind-image');
+  const heroMountainsFront = document.querySelector('.hero__mountains-front-image');
+
+  document.addEventListener('scroll', () => {
+    heroTitle.style.transform = `translate(-${scrollY * 1.25}px, ${scrollY * 0.25}px)`;
+    heroLearnButton.style.transform = `translate(-50%, ${scrollY * 0.25}px)`;
+    heroStars.style.transform = `translate(0, ${scrollY * 0.75}px)`;
+    heroMoon.style.transform = `translateY(${scrollY}px)`;
+    heroMountainsBehind.style.transform = `translate(0, ${scrollY * 0.25}px)`;
+    heroMountainsFront.style.transform = `translate(0, ${scrollY * 0.375}px)`;
+  });
+};
+
+export default hero;
