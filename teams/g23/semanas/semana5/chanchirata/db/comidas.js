a2bac775163ffd1eca5ba9fda91aa5eb2f425b26
elliotgaramendi <elliotgaramendi@gmail.com>
2023-10-02 21:59:56 -0500

    feat: create G23-5-5 comidas db

---
+++ teams/g23/semanas/semana5/chanchirata/db/comidas.js
@@ -0,0 +1,68 @@
+export const comidas = [
+  {
+    "id": 1,
+    "title": "Ají de Gallina",
+    "description": "El ají de gallina es un plato típico de la gastronomía peruana, concretamente de la costa, ​ que consiste en un ají o crema espesa con pechuga de gallina deshilachada. Esta crema es servida con papas cocidas, y en ocasiones con arroz blanco. Es común reemplazar la gallina por pollo.",
+    "urlImage": "https://imgmedia.buenazo.pe/600x338/buenazo/original/2023/06/26/6499bf204fb28f54f91a9289.png",
+    "urlRecipe": "https://diariodegastronomia.com/aji-gallina-receta-peruana/",
+    "tags": [
+      "Ají de Gallina",
+      "Ají de Pollo"
+    ]
+  },
+  {
+    "id": 2,
+    "title": "Lomo Saltado",
+    "description": "El lomo saltado es un plato típico de la gastronomía del Perú consistente en carne de res, arroz cocido y papas fritas. Es uno de los platos más consumidos popularmente en el Perú.​",
+    "urlImage": "https://upload.wikimedia.org/wikipedia/commons/c/ca/Lomo-saltado-perudelights.jpg",
+    "urlRecipe": "https://www.quericavida.com/recetas/lomo-saltado/5ea8ace7-4c81-4bf7-930d-fe69322febaf",
+    "tags": [
+      "Lomo Saltado",
+      "Pollo Saltado"
+    ]
+  },
+  {
+    "id": 3,
+    "title": "Chaufa",
+    "description": "Arroz chaufa también conocido como Arroz de chaufa es un plato de arroz frito chino. Es un plato al estilo chifa, una cocina china. Consiste en una mezcla de arroz frito con verduras, que generalmente incluye cebolletas, huevos y pollo, cocinado rápidamente a fuego alto, a menudo en un wok con salsa de soja y aceite.",
+    "urlImage": "https://1.bp.blogspot.com/-QSWz-_-EEBI/WYtPeiMRJrI/AAAAAAAAKFE/ES3qAmSsjDQYfggvdDG1jBcl4j4E91qugCLcBGAs/s1600/arroz-chaufa.jpg",
+    "urlRecipe": "https://www.ajinomoto.com.pe/receta/arroz-chaufa-como-en-el-chifa",
+    "tags": [
+      "Chaufa",
+      "Aeropuerto"
+    ]
+  },
+  {
+    "id": 4,
+    "title": "Frejoles",
+    "description": "El tacu tacu, es un plato peruano producto de la mixtura de la cocina de la diáspora Africana en el Perú y la cocina Peruana. Su origen proviene de la necesidad de usar la comida sobrante y no desperdicia...rla. La inmigración forzada de africanos que trabajaban de manera esclavizada en las grandes plantaciones costeñas; es donde se origina el punto de conexión entre la cocina africana, que mezclada a los potajes y aderezos peruanos.",
+    "urlImage": "https://recetascocinaperuana.com/wp-content/uploads/2022/03/frejoles-canarios-peruanos.jpg",
+    "urlRecipe": "https://www.recetasnestle.com.pe/recetas/main-course/frejoles",
+    "tags": [
+      "Frejoles",
+      "Frijoles"
+    ]
+  },
+  {
+    "id": 5,
+    "title": "Pollo a la Brasa",
+    "description": "Pollo a la brasa es la denominación que se da en el Perú al pollo asado al carbón, a la leña o a gas en un sistema rotatorio.​ Es considerado un platillo típico de la gastronomía peruana y uno de los de mayor consumo, incluso por encima del ceviche, el chifa y las especialidades de la comida rápida.",
+    "urlImage": "https://i1.wp.com/actualidadavipecuaria.com/wp-content/uploads/2020/03/pollo-a-la-brasa-rico-peru.jpg",
+    "urlRecipe": "https://sibarita.pe/receta/pollo-a-la-brasa-casero/",
+    "tags": [
+      "Pollo a la Brasa",
+      "Pollo a la Leña"
+    ]
+  },
+  {
+    "id": 6,
+    "title": "Tallarin Saltado",
+    "description": "El tallarín saltado es un plato típico de la gastronomía peruana, propio de la carta de un restaurante chifa así como el arroz chaufa y el wantán frito. El nombre del plato proviene de la técnica conocida como «saltear», en la cual se fríe la comida a fuego fuerte y en pequeños trozos.​",
+    "urlImage": "https://www.deliciosi.com/images/1400/1414/tallar%C3%ADn-saltado-de-pollo.jpg",
+    "urlRecipe": "https://comidasperuanas.net/tallarin-saltado-criollo/",
+    "tags": [
+      "Tallarin Saltado",
+      "Tallarin Saltado Criollo"
+    ]
+  }
+];
