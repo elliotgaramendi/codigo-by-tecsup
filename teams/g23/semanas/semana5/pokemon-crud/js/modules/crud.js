f5d4288c70c07232997d792c48a571ca127f4f56
elliotgaramendi <elliotgaramendi@gmail.com>
2023-10-03 22:00:19 -0500

    feat: create g23-5-6 read pokemon

--- teams/g23/semanas/semana5/pokemon-crud/js/modules/crud.js
+++ teams/g23/semanas/semana5/pokemon-crud/js/modules/crud.js
@@ -12,31 +12,71 @@ const crud = () => {
     const image = pokemonForm['image'].value;
 
     pokemons = [...pokemons, { name, type, hp, basic, special, image }];
+    readPokemon();
+    pokemonForm.reset();
+  };
 
+  const readPokemon = () => {
     pokemonTBody.innerHTML = '';
 
     pokemons.forEach((element, index) => {
       const { name, type, hp, basic, special, image } = element;
-      pokemonTBody.innerHTML += `
-        <tr>
-          <td>${index + 1}</td>
-          <td>${name}</td>
-          <td>${type}</td>
-          <td>${hp}</td>
-          <td>${basic}</td>
-          <td>${special}</td>
-          <td>
-            <img src="${image}" alt="${name}" width="48" height="48" />
-          </td>
-          <td>
-            <button class="btn btn-outline-danger btn-sm">🗑</button>
-            <button class="btn btn-outline-warning btn-sm">✏</button>
-          </td>
-        </tr>
-      `;
-    });
+      // pokemonTBody.innerHTML += `
+      //   <tr>
+      //     <td>${index + 1}</td>
+      //     <td>${name}</td>
+      //     <td>${type}</td>
+      //     <td>${hp}</td>
+      //     <td>${basic}</td>
+      //     <td>${special}</td>
+      //     <td>
+      //       <img src="${image}" alt="${name}" width="48" height="48" />
+      //     </td>
+      //     <td>
+      //       <button class="btn btn-outline-danger btn-sm">🗑</button>
+      //       <button class="btn btn-outline-warning btn-sm">✏</button>
+      //     </td>
+      //   </tr>
+      // `;
 
-    pokemonForm.reset();
+      const tableRow = document.createElement('tr');
+
+      const idTableData = document.createElement('td');
+      idTableData.textContent = index + 1;
+      const nameTableData = document.createElement('td');
+      nameTableData.textContent = name;
+      const typeTableData = document.createElement('td');
+      typeTableData.textContent = type;
+      const hpTableData = document.createElement('td');
+      hpTableData.textContent = hp;
+      const basicTableData = document.createElement('td');
+      basicTableData.textContent = basic;
+      const specialTableData = document.createElement('td');
+      specialTableData.textContent = special;
+      const imageTableData = document.createElement('td');
+      const imageImg = document.createElement('img');
+      imageImg.setAttribute('src', image);
+      imageImg.setAttribute('alt', name);
+      imageImg.setAttribute('width', '48px');
+      imageImg.setAttribute('height', '48px');
+      imageTableData.appendChild(imageImg);
+
+      const actionsTableData = document.createElement('td');
+      const deleteButton = document.createElement('button');
+      deleteButton.classList.add('btn', 'btn-outline-danger', 'btn-sm');
+      deleteButton.textContent = '✏';
+      actionsTableData.appendChild(deleteButton);
+
+      tableRow.appendChild(idTableData);
+      tableRow.appendChild(nameTableData);
+      tableRow.appendChild(typeTableData);
+      tableRow.appendChild(hpTableData);
+      tableRow.appendChild(basicTableData);
+      tableRow.appendChild(specialTableData);
+      tableRow.appendChild(imageTableData);
+      tableRow.appendChild(actionsTableData);
+      pokemonTBody.appendChild(tableRow);
+    });
   };
 
   pokemonForm.addEventListener('submit', e => {
