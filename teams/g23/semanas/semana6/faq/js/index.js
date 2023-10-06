dbf37a9df0ff41ad563f4994ed21e5fc31cf7850
elliotgaramendi <elliotgaramendi@gmail.com>
2023-10-05 20:26:07 -0500

    feat: create G23-6-1 FAQ js

---
+++ teams/g23/semanas/semana6/faq/js/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import FAQ from './modules/faq.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  FAQ();
+});
---
+++ teams/g23/semanas/semana6/faq/js/modules/faq.js
@@ -0,0 +1,25 @@
+const FAQ = () => {
+  const categories = document.getElementById('categories');
+
+  const fetchData = async () => {
+    const response = await fetch('./json/database.json');
+    const data = await response.json();
+
+    data.forEach(element => {
+      const { id, image, title, slug, text } = element;
+      categories.innerHTML += `
+        <div class="card" key=${id}>
+          <div class="card__body">
+            <img src="${image}" width="48px" height="48px" />
+            <h2 class="card__title">${title}</h2>
+            <p class="card__text">${text}</p>
+            <a href="${slug}" class="button button--primary">Ver preguntas</a>
+          </div>
+        </div>
+      `;
+    });
+  };
+  fetchData();
+};
+
+export default FAQ;
