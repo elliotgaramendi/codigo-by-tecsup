dbe09d584cfee6e54aaf59ffca75a9f505962dfa
elliotgaramendi <elliotgaramendi@gmail.com>
2023-10-06 21:25:52 -0500

    feat: create G23-6-2 data fetching

--- teams/g23/semanas/semana6/happy-students/js/index.js
+++ teams/g23/semanas/semana6/happy-students/js/index.js
@@ -1,3 +1,12 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => { });
+document.addEventListener('DOMContentLoaded', () => {
+  const students = document.getElementById('students');
+
+  const fetchData = async () => {
+    const response = await fetch('./json/students.json');
+    const data = await response.json();
+    console.log(data);
+  };
+  fetchData();
+});
