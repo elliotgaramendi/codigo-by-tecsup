cbd424d07010de2cb9716241ea00737dbd428906
elliotgaramendi <elliotgaramendi@gmail.com>
2023-10-07 10:12:39 -0500

    feat: create G23-6-3 header theme change

--- teams/g23/semanas/semana6/happy-pokemons/js/modules/haeder.js
+++ teams/g23/semanas/semana6/happy-pokemons/js/modules/haeder.js
@@ -1,10 +1,16 @@
 const header = () => {
   const header = document.querySelector('.header');
   const headerNav = document.querySelector('.header-nav');
+  const headerNavThemeIconContainer = document.getElementById('headerNavThemeIconContainer');
 
   window.addEventListener('scroll', () => {
     header.classList.toggle('header--scroll', scrollY > 0);
     headerNav.classList.toggle('header-nav--scroll', scrollY > 0);
+  });
+
+  headerNavThemeIconContainer.addEventListener('click', () => {
+    document.documentElement.classList.toggle('light');
+    headerNavThemeIconContainer.classList.toggle('header-nav__theme-icon-container--active');
   });
 };
 
