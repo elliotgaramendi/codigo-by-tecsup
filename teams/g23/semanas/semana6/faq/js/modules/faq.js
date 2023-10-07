e476214fbbb5e9edbad941c33df17f4016a73833
elliotgaramendi <elliotgaramendi@gmail.com>
2023-10-06 21:00:04 -0500

    feat: update G23-6-1 js

--- teams/g23/semanas/semana6/faq/js/modules/faq.js
+++ teams/g23/semanas/semana6/faq/js/modules/faq.js
@@ -1,25 +1,29 @@
-// const FAQ = () => {
-//   const categories = document.getElementById('categories');
+const FAQ = () => {
+  const categories = document.getElementById('categories');
 
-//   const fetchData = async () => {
-//     const response = await fetch('./json/database.json');
-//     const data = await response.json();
+  const fetchData = async () => {
+    const response = await fetch('./json/database.json');
+    const data = await response.json();
 
-//     data.forEach(element => {
-//       const { id, image, title, slug, text } = element;
-//       categories.innerHTML += `
-//         <div class="card" key=${id}>
-//           <div class="card__body">
-//             <img src="${image}" width="48px" height="48px" />
-//             <h2 class="card__title">${title}</h2>
-//             <p class="card__text">${text}</p>
-//             <a href="${slug}" class="button button--primary">Ver preguntas</a>
-//           </div>
-//         </div>
-//       `;
-//     });
-//   };
-//   fetchData();
-// };
+    data.forEach(element => {
+      const { id, image, title, slug, text } = element;
+      categories.innerHTML += `
+        <div class="card card--flip" key="${id}">
+          <div class="card__body card__body--flip">
+            <img
+              src="${image}"
+              width="48px" height="48px" class="img img--category"/>
+            <h2 class="card__title">${title}</h2>
+          </div>
+          <div class="card__body card__body--flip">
+            <p class="card__text">${text}</p>
+            <a href="${slug}" class="button button--primary">Ver preguntas</a>
+          </div>
+        </div>
+      `;
+    });
+  };
+  fetchData();
+};
 
-// export default FAQ;
+export default FAQ;
