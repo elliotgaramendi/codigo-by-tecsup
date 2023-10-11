56c2aa7f658312a85e50239e0c9c0f99f018793a
elliotgaramendi <elliotgaramendi@gmail.com>
2023-10-10 22:14:24 -0500

    feat: create G23-7-1 grid layout styles

--- teams/g23/semanas/semana7/help-center/src/components/Hero.jsx
+++ teams/g23/semanas/semana7/help-center/src/components/Hero.jsx
@@ -8,9 +8,14 @@ const Hero = () => {
         height={1080}
         className="img img--hero-background"
       />
-      <div className="container">
-        <h1>Centro de ayuda</h1>
-        <h2>Podrás navegar por nuestras categorías y buscar información para resolver tus consultas.</h2>
+      <div className="container g-layout g-layout--center md:g-layout--1fr-2fr">
+        <div>
+          <h1>Centro de ayuda</h1>
+          <h2>Podrás navegar por nuestras categorías y buscar información para resolver tus consultas.</h2>
+        </div>
+        <form>
+          <input type="search" name="question" placeholder="Pregunta" />
+        </form>
       </div>
     </section>
   );
