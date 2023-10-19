46d8acdc553ef2fc20c0a3dcf382cf8104594e1c
elliotgaramendi <elliotgaramendi@gmail.com>
2023-10-18 22:25:09 -0500

    feat: create G23-8-2 fetchData

--- teams/g23/semanas/semana8/rick-and-morty/src/App.jsx
+++ teams/g23/semanas/semana8/rick-and-morty/src/App.jsx
@@ -1,8 +1,26 @@
+import { useEffect } from "react";
 import Hero from "./components/modules/Hero";
+import { useState } from "react";
 
 function App() {
+  const heroData = {
+    title: 'Rick and Morty',
+    image: 'https://images.alphacoders.com/133/thumb-1920-1330376.png'
+  };
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://rickandmortyapi.com/api/character');
+      const data = await response.json();
+      setData(data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <Hero />
+    <Hero data={heroData} />
   );
 }
 
--- teams/g23/semanas/semana8/rick-and-morty/src/components/modules/Hero.jsx
+++ teams/g23/semanas/semana8/rick-and-morty/src/components/modules/Hero.jsx
@@ -1,15 +1,16 @@
-const Hero = () => {
+/* eslint-disable react/prop-types */
+const Hero = ({ data: { title, image } }) => {
   return (
     <section className="relative min-h-[37.5vh] flex">
       <img
-        src="https://images.alphacoders.com/133/thumb-1920-1330376.png"
-        alt="Rick and Morty"
+        src={image}
+        alt={title}
         width={1920}
         height={1080}
         className="absolute -z-10 w-full h-full object-cover opacity-75"
       />
       <div className="container px-2 mx-auto flex items-center justify-center">
-        <h1 className="text-5xl text-center font-black">Rick and Morty</h1>
+        <h1 className="text-5xl text-center font-black">{title}</h1>
       </div>
     </section>
   );
