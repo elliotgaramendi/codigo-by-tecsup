431bcd2475414e5cecc2ca873b831fbd18a659d7
elliotgaramendi <elliotgaramendi@gmail.com>
2023-10-21 10:01:44 -0500

    feat: update G23-7-1 Header searchOffCanvas

--- teams/g23/semanas/semana7/help-center/src/layouts/PrimaryLayout.jsx
+++ teams/g23/semanas/semana7/help-center/src/layouts/PrimaryLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Header from '../components/Header';
+import Header from '../components/layouts/Header';
 
 const PrimaryLayout = () => {
   return (
