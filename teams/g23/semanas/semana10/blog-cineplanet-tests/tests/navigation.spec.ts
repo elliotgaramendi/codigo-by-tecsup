77bc53bb0c88d2a9aa14c178994a1e7f9dc0fb58
elliotgaramendi <elliotgaramendi@gmail.com>
2023-11-01 21:49:29 -0500

    feat: Create G23-10-2 Header navigation

--- teams/g23/semanas/semana10/blog-cineplanet-tests/tests/navigation.spec.ts
+++ teams/g23/semanas/semana10/blog-cineplanet-tests/tests/navigation.spec.ts
@@ -4,9 +4,22 @@ test.beforeEach(async ({ page }) => {
   await page.goto(process.env.URL!);
 });
 
-test.describe('Blog Cineplanet entry tests', () => {
+test.describe('Blog Cineplanet Entry Tests', () => {
   test('Should have the correct titles', async ({ page }) => {
     await expect(page).toHaveTitle(/Blog Cineplanet/);
+    await expect(page.getByRole('heading', { name: 'Blog Cineplanet I Lo mejor del cine y entretenimiento' })).toBeVisible();
+  });
+});
+
+test.describe('Header navigation', () => {
+  test('Should allow navigation and display of title', async ({ page }) => {
+    await page.getByRole('link', { name: 'Noticias' }).click();
+    await expect(page.getByRole('heading', { name: 'Noticias planet' })).toBeVisible();
+    await page.getByRole('link', { name: 'Notas' }).click();
+    await expect(page.getByRole('heading', { name: 'Notas planet' })).toBeVisible();
+    await page.getByRole('link', { name: 'Videos' }).click();
+    await expect(page.getByRole('heading', { name: 'Videos planet' })).toBeVisible();
+    await page.getByRole('link', { name: 'Blog Cineplanet' }).click();
     await expect(page.getByRole('heading', { name: 'Blog Cineplanet I Lo mejor del cine y entretenimiento' })).toBeVisible();
   });
 });
