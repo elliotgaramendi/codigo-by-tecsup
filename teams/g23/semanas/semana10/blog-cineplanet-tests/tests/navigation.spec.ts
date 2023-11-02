4f2ea147e9bf7142b426c8d67f1c9d8a0f280bdb
elliotgaramendi <elliotgaramendi@gmail.com>
2023-11-01 22:01:00 -0500

    feat: Update G23-10-2 Header navigation

--- teams/g23/semanas/semana10/blog-cineplanet-tests/tests/navigation.spec.ts
+++ teams/g23/semanas/semana10/blog-cineplanet-tests/tests/navigation.spec.ts
@@ -12,7 +12,7 @@ test.describe('Blog Cineplanet Entry Tests', () => {
 });
 
 test.describe('Header navigation', () => {
-  test('Should allow navigation and display of title', async ({ page }) => {
+  test('Should allow internal navigation and  of title', async ({ page }) => {
     await page.getByRole('link', { name: 'Noticias' }).click();
     await expect(page.getByRole('heading', { name: 'Noticias planet' })).toBeVisible();
     await page.getByRole('link', { name: 'Notas' }).click();
@@ -21,5 +21,13 @@ test.describe('Header navigation', () => {
     await expect(page.getByRole('heading', { name: 'Videos planet' })).toBeVisible();
     await page.getByRole('link', { name: 'Blog Cineplanet' }).click();
     await expect(page.getByRole('heading', { name: 'Blog Cineplanet I Lo mejor del cine y entretenimiento' })).toBeVisible();
+  });
+
+  test('Should allow navigation to movies and view their title', async ({ page }) => {
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByRole('banner').getByRole('link', { name: 'Cartelera' }).click();
+    const page1 = await page1Promise;
+    await page1.getByRole('button', { name: 'Aceptar Cookies' }).click();
+    await page1.getByRole('heading', { name: 'Películas' }).locator('span').click();
   });
 });
