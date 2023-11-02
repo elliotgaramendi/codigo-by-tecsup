dd848c23549bbf3b5e604bd2ee3eabda4ff28780
elliotgaramendi <elliotgaramendi@gmail.com>
2023-11-01 21:04:19 -0500

    feat: Create G23-10-2 Web entry test failed

--- teams/g23/semanas/semana10/blog-cineplanet-tests/tests/navigation.spec.ts
+++ teams/g23/semanas/semana10/blog-cineplanet-tests/tests/navigation.spec.ts
@@ -1,5 +1,11 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto(process.env.URL);
+});
+
+test.describe('Web entry test', () => {
+  test('Has title', async ({page}) => {
+    await expect(page).toHaveTitle(/Cineplanet Perú/);
+  });
 });
