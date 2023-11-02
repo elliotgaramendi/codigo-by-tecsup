7280484dbb445317b84437754e6b998a048c0e11
elliotgaramendi <elliotgaramendi@gmail.com>
2023-11-01 21:10:30 -0500

    refactor: Improve G23-10-2 entry tests names

--- teams/g23/semanas/semana10/blog-cineplanet-tests/tests/navigation.spec.ts
+++ teams/g23/semanas/semana10/blog-cineplanet-tests/tests/navigation.spec.ts
@@ -1,11 +1,12 @@
 import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto(process.env.URL);
+  await page.goto(process.env.URL!);
 });
 
-test.describe('Web entry test', () => {
-  test('Has title', async ({page}) => {
+test.describe('Blog Cineplanet entry tests', () => {
+  test('Should have the correct titles', async ({ page }) => {
     await expect(page).toHaveTitle(/Blog Cineplanet/);
+    await expect(page.getByRole('heading', { name: 'Blog Cineplanet I Lo mejor del cine y entretenimiento' })).toBeVisible();
   });
 });
