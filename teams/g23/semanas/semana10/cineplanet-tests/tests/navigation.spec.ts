997b1b52aae672f8bb2312768651f55f634facef
elliotgaramendi <elliotgaramendi@gmail.com>
2023-11-02 21:51:32 -0500

    test: Create G23-10-3 Entry tests

--- teams/g23/semanas/semana10/cineplanet-tests/tests/navigation.spec.ts
+++ teams/g23/semanas/semana10/cineplanet-tests/tests/navigation.spec.ts
@@ -1,5 +1,12 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto(process.env.URL!)
+});
+
+test.describe('Entry Tests', () => {
+  test('Should have the correct titles', async ({ page }) => {
+    await expect(page).toHaveTitle(/Cineplanet/);
+    await expect(page.getByRole('heading', { name: 'Películas' })).toBeVisible();
+  });
 });
