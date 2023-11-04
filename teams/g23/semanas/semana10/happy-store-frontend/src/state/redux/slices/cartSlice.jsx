b87f36597482846dbb9a6571630e25101d8cae3b
elliotgaramendi <elliotgaramendi@gmail.com>
2023-11-04 10:03:08 -0500

    feat: Update G23-10-1 cartSlice reducers

--- teams/g23/semanas/semana10/happy-store-frontend/src/state/redux/slices/cartSlice.jsx
+++ teams/g23/semanas/semana10/happy-store-frontend/src/state/redux/slices/cartSlice.jsx
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   cartItems: JSON.parse(localStorage.getItem('cartItems')) || [],
@@ -7,8 +7,42 @@ const initialState = {
 export const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducer: {}
+  reducer: {
+    addToCart: (state, action) => {
+      const { cartItems } = state;
+      const { payload } = action;
+      if (cartItems.findIndex(element => element.id === payload.id) === -1) {
+        return ({
+          ...state,
+          cartItems: [...cartItems, { ...payload, quantity: 1 }]
+        });
+      } else {
+        return ({
+          ...state,
+          cartItems: cartItems.map(element => element.id !== payload.id ? element : { ...element, quantity: element.quantity + 1 })
+        });
+      }
+    },
+    removeFromCart: (state, action) => {
+      const { cartItems } = state;
+      const { payload } = action;
+      return ({
+        ...state,
+        cartItems: cartItems.filter(element => element.id !== payload.id)
+      });
+    },
+    updateQuantity: (state, action) => {
+      const { cartItems } = state;
+      const { payload } = action;
+      return ({
+        ...state,
+        cartItems: cartItems.map(element => {
+          return element.id !== payload.element.id ? element : { ...element, quantity: changeValue(element.quantity, payload.value) };
+        })
+      });
+    }
+  }
 });
 
-export const { } = cartSlice.actions;
+export const { addToCart, removeFromCart, updateQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
