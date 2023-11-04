11125459287f4438c2f3543220c0508a1f50b67a
elliotgaramendi <elliotgaramendi@gmail.com>
2023-11-03 22:36:34 -0500

    feat: update G23-9-2 Cart

--- teams/g23/semanas/semana9/happy-store-frontend/src/components/cart/Cart.jsx
+++ teams/g23/semanas/semana9/happy-store-frontend/src/components/cart/Cart.jsx
@@ -4,8 +4,14 @@ import CartList from "./CartList";
 
 const Cart = () => {
   return (
-    <div className="fixed z-50 top-0 right-0 bottom-0 left-0 bg-zinc-900/50 flex justify-end opacity-0 pointer-events-none transition-opacity target:opacity-100 target:pointer-events-auto" id="cartOffCanvas">
-      <div className="relative w-3/4 max-w-lg h-full p-4 bg-zinc-900/50 flex flex-col gap-4 ">
+    <div
+      onClick={() => window.location.hash = ''}
+      className="fixed z-50 top-0 right-0 bottom-0 left-0 bg-zinc-900/50 flex justify-end opacity-0 pointer-events-none transition-opacity target:opacity-100 target:pointer-events-auto" id="cartOffCanvas"
+    >
+      <div
+        className="relative w-3/4 max-w-lg h-full p-4 bg-zinc-900/50 flex flex-col gap-4 "
+        onClick={e => e.stopPropagation()}
+      >
         <CartHeader />
         <CartList />
         <CartFooter />
