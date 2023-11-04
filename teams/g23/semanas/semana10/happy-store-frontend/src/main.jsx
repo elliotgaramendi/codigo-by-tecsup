3aa7d586145770772d1618693329718cb9fe906a
elliotgaramendi <elliotgaramendi@gmail.com>
2023-11-04 09:36:28 -0500

    chore: Configure G23-10-1 Redux Toolkit store

--- teams/g23/semanas/semana10/happy-store-frontend/src/main.jsx
+++ teams/g23/semanas/semana10/happy-store-frontend/src/main.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App.jsx';
 import './index.css';
 import { CartProvider } from './state/context/CartContext.jsx';
 import { ProductProvider } from './state/context/ProductContext.jsx';
+import { store } from './state/redux/store.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ProductProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </ProductProvider>
+    <Provider store={store}>
+      <ProductProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ProductProvider>
+    </Provider>
   </React.StrictMode>,
 );
